9ed569147e2ffef7b572e18b55adbfdd9e8a6e92
Casey Cross <hellocaseycross@gmail.com>
2016-07-12 14:58:45 -0600

    Added preliminary JS variables and event listeners.

---
+++ script.js
@@ -0,0 +1,18 @@
+var submitButton = document.querySelector('.submit-button');
+var readButton = document.querySelector('.read-button');
+var removeButton = document.querySelector('.remove-button');
+var titleInput = document.querySelector('.title-input');
+var urlInput = document.querySelector('.url-input');
+
+// Submit Button functions
+submitButton.addEventListener('click', function(){
+
+});
+
+submitButton.addEventListener('click', function(){
+
+});
+
+submitButton.addEventListener('click', function(){
+
+});
