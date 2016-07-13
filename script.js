4dbeea6c167fbfc1c773c9024fc1bc1eb3e1e635
Casey Cross <hellocaseycross@gmail.com>
2016-07-12 19:05:06 -0600

    Append new article with user input.

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 // Practicing some jquery
 
 $('.submit-button').on('click', function() {
-  var linkTitle = $('.title-input').val();
-  $('.link-title').text(linkTitle);
-  var urlTitle = $('.url-input').val();
-  $('.url-title').text(urlTitle);
+
+
+  // var linkTitle = $('.title-input').val();
+  // $('.link-title').text(linkTitle);
+  // var urlTitle = $('.url-input').val();
+  // $('.url-title').text(urlTitle);
+
+
+  $('.container').append('<article><p class="link-title">' + $('.title-input').val() + '</p><p class="url-title">' + $('.url-input').val() + '</p><div class="button-group"><button class="read-button">Mark as Read</button><button class="remove-button">Remove</button><div></article>');
+
 });
