a1bbb0c97a8d2bb5655c90968236a8b7c578a6d0
Ben Godfrey <redzone41@gmail.com>
2016-07-13 11:15:27 -0600

    Add error messages when user does not input Title, url, or both.

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+// var titleInput = document.querySelector('.title-input');
+// var urlInput = document.querySelector('.url-input');
+// var infoSection = document.querySelector('.info');
+
+// function checkTitleInput() {
+//   if(titleInput.value = '') {
+//     infoSection.innerText = 'Please enter a title for your link.';
+//   }
+// }
+//
+// function checkUrlInput() {
+//   if(urlInput.value = '') {
+//     infoSection.innerText = 'Please enter a URL for your link.';
+//   }
+// }
+// var infoSection = $('.info');
+// var titleInput = $('.title-input');
+// var urlInput = $('.url-input');
 
 // Adds Articles to List
 
 $('.submit-button').on('click', function() {
-  $('.container').append('<article><p class="link-title">' + $('.title-input').val() + '</p><p class="url-title">' + $('.url-input').val() + '</p><div class="button-group"><button class="read-button">Mark as Read</button><button class="remove-button">Remove</button><div></article>');
+  var infoSection = $('.info');
+  var titleInput = $('.title-input');
+  var urlInput = $('.url-input');
+
+  if($(urlInput).val()==='' && $(titleInput).val()==='') {
+    infoSection.text('wut');
+  } else if($(titleInput).val()==='') {
+  infoSection.text('Please enter a title for your link.');
+} else if($(urlInput).val()==='') {
+  infoSection.text('Please enter a valid URL.');
+} else {
+
+  $('.container').append('<article><p class="link-title">' +
+  $('.title-input').val() +
+  '</p><p class="url-title">' +
+  $('.url-input').val() +
+  '</p><div class="button-group"><button class="read-button">Mark as Read</button><button class="remove-button">Remove</button></div></article>');
 //Toggles Read Class when Read Button is clicked
   $('.read-button').on('click', function() {
       $(this).parent().parent().toggleClass('read');
@@ -10,5 +44,11 @@ $('.submit-button').on('click', function() {
 //Removes when Remove Button is clicked
   $('.remove-button').on('click', function() {
       $(this).parent().parent().remove('article');
-    });
+    });}
   });
+
+// if (titleInput.value='') {
+//   infoSection.innerText = 'Please enter a title for your link.';
+// } else if (userInput.value='') {
+//   infoSection.innerText = 'Please enter a valid URL.';
+// };
