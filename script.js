275a6cd74ef93f538a4fc3924e03b4c922f9cab6
Casey Cross <hellocaseycross@gmail.com>
2016-07-13 13:16:35 -0600

    Clean up code and indents.

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function checkInputs() {
   }
 }
 
-
 function disableSubmit() {
   urlInput.keyup(checkInputs);
   titleInput.keyup(checkInputs);
@@ -18,39 +17,35 @@ function disableSubmit() {
 
 disableSubmit();
 
+submitButton.on('click', function() {
 
-$('.submit-button').on('click', function() {
-  // Return error messages
-  var infoSection = $('.info');
-  var titleInput = $('.title-input');
-  var urlInput = $('.url-input');
-
-  if(urlInput.val()==='' && titleInput.val()==='') {
+  if (urlInput.val()==='' && titleInput.val()==='') {
     infoSection.text('Really? 🙄');
-  } else if(titleInput.val()==='') {
+  } else if (titleInput.val()==='') {
   infoSection.text('Please enter a title for your link.');
-} else if(urlInput.val()==='') {
-  infoSection.text('Please enter a valid URL.');
-} else {
+  } else if (urlInput.val()==='') {
+    infoSection.text('Please enter a valid URL.');
+  } else {
 
 // Adds Articles to List
-  $('.container').append('<article><p class="link-title">' +
-  $('.title-input').val() +
-  '</p><p class="url-title">' +
-  $('.url-input').val() +
-  '</p><div class="button-group"><button class="read-button">Mark as Read</button><button class="remove-button">Remove</button></div></article>');
-//Toggles Read Class when Read Button is clicked
-  $('.read-button').on('click', function() {
+    $('.container').append(
+      '<article>' +
+        '<p class="link-title">' + titleInput.val() +'</p>' +
+        '<p class="url-title">' + urlInput.val() + '</p>' +
+        '<div class="button-group">' +
+          '<button class="read-button">Mark as Read</button>' +
+          '<button class="remove-button">Remove</button>'+
+        '</div>' +
+      '</article>'
+    );
+
+  //Toggles Read Class when Read Button is clicked
+    $('.read-button').on('click', function() {
       $(this).parent().parent().toggleClass('read');
     });
-//Removes when Remove Button is clicked
-  $('.remove-button').on('click', function() {
+  //Removes when Remove Button is clicked
+    $('.remove-button').on('click', function() {
       $(this).parent().parent().remove('article');
-    });}
-  });
-
-// if (titleInput.value='') {
-//   infoSection.innerText = 'Please enter a title for your link.';
-// } else if (userInput.value='') {
-//   infoSection.innerText = 'Please enter a valid URL.';
-// };
+    });
+  }
+});
